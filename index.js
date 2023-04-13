924e15378cbe907f45b4bab8c5ff3b8b21db3d5e
Muneeb <muneeb.mughal@mobilelive.ca>
2023-04-13 23:38:55 +0500

    Auto deploment added

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 app.post('/api', async (req, res) => {
     res.status(200).json({
         status: true,
-        message: 'Hello World'
+        message: 'Hello World!'
     })
 })
 
