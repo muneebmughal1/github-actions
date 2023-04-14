1c798565b7c5eddd5fa3da7b8531a645066c09d3
Muneeb <muneeb.mughal@mobilelive.ca>
2023-04-14 16:57:16 +0500

    sample

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 app.get('/api', async (req, res) => {
     res.status(200).json({
         status: true,
-        message: 'Hello World!'
+        message: 'Hello World!. Updated.'
     })
 })
 
