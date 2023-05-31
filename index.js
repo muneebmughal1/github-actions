01dc554479a85e359dde7cbc22824eb6146dee55
Muneeb <muneeb.mughal@mobilelive.ca>
2023-05-31 16:50:32 +0500

    test

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const app = require('express')();
-const bodyParser = require('body-parser');
+// const app = require('express')();
+// const bodyParser = require('body-parser');
 
-const PORT = process.env.PORT || 4000;
-app.use(bodyParser.json());
+// const PORT = process.env.PORT || 4000;
+// app.use(bodyParser.json());
 
-app.get('/api', async (req, res) => {
-    res.status(200).json({
-        status: true,
-        message: 'Hello World! I am simple express API and I am deployed using github actions and my infra was created using terraform'
-    })
-})
-app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`)
-})
+// app.get('/api', async (req, res) => {
+//     res.status(200).json({
+//         status: true,
+//         message: 'Hello World! I am simple express API and I am deployed using github actions and my infra was created using terraform'
+//     })
+// })
+// app.listen(PORT, () => {
+//     console.log(`server is running on port ${PORT}`)
+// })
+console.log('Hi from Muneeb Mughal test version');
